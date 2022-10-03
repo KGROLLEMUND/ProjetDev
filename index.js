9e0e83b983f98a9deed1d0883bf0531a7aca8d98
KGROLLEMUND <74825518+KGROLLEMUND@users.noreply.github.com>
2022-10-03 20:01:22 +0200

    final

---
+++ index.js
@@ -0,0 +1,177 @@
+var createError = require('http-errors');
+var express = require('express')
+var app = express()
+var path = require('path');
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 5000
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+const cookieParser = require('cookie-parser');
+const authUtils = require('./utils/auth');
+const authRouter = require('./routes/auth');
+
+const MongoClient = require('mongodb').MongoClient;
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
+const session = require('express-session');
+const flash = require('connect-flash');
+const hbs = require('hbs');
+var logger = require('morgan');
+var indexRouter = require('./routes/index');
+
+
+app.use(express.json())
+app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(session({
+  secret: 'secret session',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.loggedIn = req.isAuthenticated();
+  next();
+});
+
+app.use(logger('dev'));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs')
+app.use(cookieParser());
+
+app.use(express.urlencoded({ extended: false }));
+hbs.registerPartials(path.join(__dirname, 'views/_partials'));
+
+http.listen(port, () => {
+  console.log('Connextion listen on 5000')
+})
+
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+  if(err) {
+    throw err;
+    
+  }
+  console.log('Database Connected...');
+  const db = client.db('SuiviProjetDev');
+  const users = db.collection('users');
+  app.locals.users = users;
+})
+
+passport.use(new Strategy(
+  (username, password, done) => {
+
+    app.locals.users.findOne({ username }, (err, user) => {
+      if(err) {
+        return done(err);
+      }
+
+      if(!user) {
+        return done(null, false);
+      }
+
+      if(user.password != authUtils.hashPassword(password )) {
+        return done(null, false);
+      }
+
+      return done(null, user);
+    })
+  }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+
+});
+
+passport.deserializeUser((id, done) => {
+done(null, { id });
+});
+
+app.use('/', indexRouter);
+
+app.use('/auth', authRouter);
+
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+
+var players = {},
+  unmatched;
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+});
+
+io.sockets.on("connection", function (socket) {
+  console.log("socket connected")
+  socket.emit('connect', {
+    msg: "hello"
+  })
+  joinGame(socket);
+
+  if (getOpponent(socket)) {
+    socket.emit("game.begin", {
+      symbol: players[socket.id].symbol,
+    });
+    getOpponent(socket).emit("game.begin", {
+      symbol: players[getOpponent(socket).id].symbol,
+    });
+  }
+
+  socket.on("make.move", function (data) {
+    if (!getOpponent(socket)) {
+      return;
+    }
+    socket.emit("move.made", data);
+    getOpponent(socket).emit("move.made", data);
+  });
+
+  socket.on("disconnect", function () {
+    if (getOpponent(socket)) {
+      getOpponent(socket).emit("opponent.left");
+    }
+  });
+});
+
+function joinGame(socket) {
+  players[socket.id] = {
+    opponent: unmatched,
+
+    symbol: "X",
+    // The socket that is associated with this player
+    socket: socket,
+  };
+  if (unmatched) {
+    players[socket.id].symbol = "O";
+    players[unmatched].opponent = socket.id;
+    unmatched = null;
+  } else {
+    unmatched = socket.id;
+  }
+}
+
+function getOpponent(socket) {
+  if (!players[socket.id].opponent) {
+    return;
+  }
+  return players[players[socket.id].opponent].socket;
+}
+
+module.exports = app;
---
+++ routes/auth.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const router = new express.Router();
+const authUtils = require('../utils/auth');
+const passport = require('passport');
+const flash = require('connect-flash');
+
+// --------------------------------------------------
+router.get('/login', (req, res, next) => {
+  if (req.isAuthenticated()) { 
+    res.redirect('/');
+  }
+  const messages = req.flash();
+  res.render('login', { messages });
+});
+// --------------------------------------------------
+// Handle login request
+// --------------------------------------------------
+router.post('/login', passport.authenticate('local', 
+  { failureRedirect: '/auth/login', 
+    failureFlash: 'Wrong username or password'}), (req, res, next) => {
+  res.redirect('/');
+});
+// --------------------------------------------------
+
+
+router.get('/register', (req, res, next) => {
+  if (req.isAuthenticated()) { 
+    res.redirect('/');
+  }
+  const messages = req.flash();
+  
+  res.render('register', { messages });
+});
+  // --------------------------------------------------
+  
+  
+  // Handle register request
+  // --------------------------------------------------
+  router.post('/register', (req, res, next) => {
+    
+    const registrationParams = req.body;
+    const users = req.app.locals.users;
+    console.log('take2');
+ 
+    const payload = {
+      username: registrationParams.username,
+      email: registrationParams.email,
+      password: authUtils.hashPassword(registrationParams.password),
+      victory: 0,
+      loses: 0,
+      draws: 0,
+    };
+    console.log('take3');
+
+   
+  
+    users.insertOne(payload, (err) => {
+      if (err) {
+        req.flash('error', 'User account already exists.');
+      } else {
+        console.log('take4');
+        req.flash('success', 'User account registered successfully.');
+      }
+  
+      res.redirect('/auth/register');
+    })
+  });
+
+  router.get('/logout', (req, res, next) => {
+    req.session.destroy();
+    res.redirect('/');
+    
+  });
+  
+  module.exports = router;
